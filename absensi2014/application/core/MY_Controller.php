42e5bb7c32e6afa0d4acb10f2f3e33685805041c
mlatifr <mlatifraldi@gmail.com>
2020-08-01 05:30:30 +0700

    v6.10.4

---
+++ absensi2014/application/core/MY_Controller.php
@@ -0,0 +1,18 @@
+<?php
+    if(!defined('BASEPATH'))
+        exit('No direct script access allowed');
+    class MY_Contorller extends CI_Controller
+    {
+        public function __construct()
+        {
+            parent::__construct();
+            session_start();
+            
+            //cek status login user
+            if ($this->session->userdata('login') == FALSE)
+            {
+                redirect('login');
+            }
+        }
+    }
+?>
